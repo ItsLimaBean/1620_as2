db378cba578faa74720c4af95a47a9b271a9a531
Liam Brooke <24947193+ItsLimaBean@users.noreply.github.com>
2022-04-05 14:34:26 -0700

    Added functionality to the new note button.

--- src/app.js
+++ src/app.js
@@ -5,3 +5,25 @@ const notes = [
     id: 1 
   }
 ]
+
+const newNoteButton = document.querySelector(".fa-solid.fa-circle-plus")
+const createNoteArea = document.querySelector(".create-note-area")
+
+const newNoteArea = `
+<div class="new-note-area">
+  <textarea placeholder='Enter note'></textarea>
+  <div>
+    <button>Save</button>
+    <button>Cancel</button>
+  </div>
+</div>
+`
+
+function addNewNote(event) {
+  if (document.querySelector(".new-note-area")) {
+    return
+  }
+  createNoteArea.insertAdjacentHTML("beforeend", newNoteArea)
+}
+
+newNoteButton.addEventListener("click", addNewNote)
